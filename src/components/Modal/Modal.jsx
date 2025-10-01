070e2cc7732f1f4a88456e110b3c10d94dbcae7f
Charles ABJ <biadjeu.charles@orange.fr>
2025-10-01 23:25:28 +0200

    Build du projet

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,7 +4,7 @@ function Modal({ isOpen, onClose, children }) {
   if (!isOpen) return null;
 
   return (
-    <div className={isOpen ? "Abj-modal is-open" : "Abj-modal"}>
+    <div className={isOpen ? "Abj-react-modal is-open" : "Abj-react-modal"}>
       <div className="overlay" onClick={onClose}></div>
       <div className="modal">
         <div className="close-button" onClick={onClose}>
